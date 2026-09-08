67ae23c8b239c67eecc32fc5382e8e46f8e035fc
MeghaA-Code <ambulemegha15@gmail.com>
2026-09-08 22:11:47 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ import dill
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 
@@ -16,6 +19,78 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    models,
+    param
+):
+    try:
+
+        report = {}
+
+        for i in range(len(list(models))):
+
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+
+            parameters = param[model_name]
+
+            # If parameters are provided, perform GridSearchCV
+            if parameters:
+
+                gs = GridSearchCV(
+                    model,
+                    parameters,
+                    cv=3,
+                    scoring="r2",
+                    n_jobs=-1
+                )
+
+                gs.fit(X_train, y_train)
+
+                # Get best model after hyperparameter tuning
+                model = gs.best_estimator_
+
+            else:
+
+                # Train model normally
+                model.fit(X_train, y_train)
+
+            # Prediction on training data
+            y_train_pred = model.predict(X_train)
+
+            # Prediction on testing data
+            y_test_pred = model.predict(X_test)
+
+            # R2 score
+            train_model_score = r2_score(
+                y_train,
+                y_train_pred
+            )
+
+            test_model_score = r2_score(
+                y_test,
+                y_test_pred
+            )
+
+            # Store testing score
+            report[model_name] = test_model_score
+
+            print(
+                f"{model_name}: "
+                f"Train R2 = {train_model_score:.4f}, "
+                f"Test R2 = {test_model_score:.4f}"
+            )
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
